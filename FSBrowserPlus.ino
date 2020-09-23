50eccf582bbfae5a5ac8c9120d68bcf5fd491260
moononournation <moononournation@gmail.com>
2020-09-23 18:16:59 +0800

    remove old function

--- FSBrowserPlus.ino
+++ FSBrowserPlus.ino
@@ -51,22 +51,6 @@ AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 AsyncEventSource events("/events");
 
-void hexdump(const void *mem, uint32_t len, uint8_t cols = 16)
-{
-  const uint8_t *src = (const uint8_t *)mem;
-  Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
-  for (uint32_t i = 0; i < len; i++)
-  {
-    if (i % cols == 0)
-    {
-      Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
-    }
-    Serial.printf("%02X ", *src);
-    src++;
-  }
-  Serial.printf("\n");
-}
-
 static uint16_t hexValue(uint8_t h)
 {
   if (h >= '0' && (h <= '9'))
